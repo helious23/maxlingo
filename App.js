011098cbb203df287536644d2545254eb5664f6f
Max16 <max16@naver.com>
2021-12-31 10:58:58 +0900

    #4.2 Manual Animations

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.TouchableOpacity`
+  background-color: skyblue;
+  width: 200px;
+  height: 200px;
+`;
 
 export default function App() {
+  const [y, setY] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+  const moveUp = () => {
+    const id = setInterval(() => setY((prev) => prev + 1), 1);
+    setIntervalId(id);
+  };
+
+  useEffect(() => {
+    if (y === 200) {
+      clearInterval(intervalId);
+    }
+  }, [y, intervalId]);
+
+  console.log("render");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Box
+        onPress={moveUp}
+        style={{
+          transform: [{ translateY: y }],
+        }}
+      />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
